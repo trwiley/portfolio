645b2e33e4ffee659311acd46507837a9fe34636
trwiley <taylor.r.wiley@gmail.com>
2022-11-29 15:20:01 -0500

    Image gallery displaying...images.

--- components/ImageGallery.vue
+++ components/ImageGallery.vue
@@ -1,23 +1,27 @@
 <template>
-    <p>{{filteredImages}}</p>
+    <div class="gallery">
+        <figure v-for="(item, index) in filteredImages">
+            <img :src="require(`../assets/${item.imgUrl}`)" class="image-style"/>
+        </figure>
+    </div>  
 </template>
 
 <script>
 import img from '../assets/data.json';
+
 export default{
     name: 'ImageGallery',
     props: ['filter'],
     data () {
         return {
             imageUrls: img.images,
-            filteredImages: []
+            filteredImages: [],
         }  
     },
    
     methods: {
         filterByTag() {
             const filtered = this.imageUrls.filter(image => image.tag === this.filter);
-            console.log(filtered);
             this.filteredImages = [...filtered];
         }
     },
@@ -28,4 +32,14 @@ export default{
 </script>
 
 <style scoped>
+    .gallery{
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 15px;
+    }
+    .image-style{
+        width: 200px;
+        height: auto;
+    }
+
 </style>
